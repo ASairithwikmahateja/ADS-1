178969fdb822a3cbac3a22d01378706fb8f2218c
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-09-26 22:44:06 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ M 3/3-sum/Solution.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+// /**
+//  * Class for solution.
+//  */
+final class Solution {
+    // /**
+    //  * Constructs the object.
+    //  */
+    private Solution() {
+
+    }
+    // /**
+    //  * main method that prints the count of pairs.
+    //  * @param      args  The arguments
+    //  */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                int k = Arrays.binarySearch(arr, -(arr[i] + arr[j]));
+                if (k > j) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
