8cd215fa7cbc02170e044e1bfeb2012609692540
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-10-06 12:51:25 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M 12/Assignment-1/Solution.java
+++ M 12/Assignment-1/Solution.java
@@ -33,9 +33,9 @@ final class Solution {
 		}
 		seat.selectionSort();
 		System.out.println(seat + "\n");
-		while(vac > 0) {
-			System.out.println(seat);
-			vac--;
-		}
+		// while(vac > 0) {
+		// 	System.out.println(seat);
+		// 	vac--;
+		// }
 	}
 }
