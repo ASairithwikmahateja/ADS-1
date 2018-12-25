6b0b7b4c8b4ca78606d144ff3d78df3d40618cd9
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-12-25 11:05:24 +0530

    "m24_a1_1: 2/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Remedial/1/Solution.java
+++ Remedial/1/Solution.java
@@ -14,12 +14,13 @@ public class Solution {
 		// System.out.println(m);
 		for (int j = 0; j < m; j++) {
 			String[] token1 = scan.nextLine().split(" ");
+			String str = sc.get(token1[1])+"";
 			switch (token1[2]) {
 				case "1":
-					System.out.println(sc.get(token1[1]));	
+					System.out.println(str.split(",")[0]);	
 				break;
 				case "2":
-					System.out.println(sc.get(token1[1]));
+					System.out.println(str.split(",")[1]);
 			}
 		}
 	}
