b480c05bc5dbdb6d621a29f8b94708b22c4209c4
ASairithwikmahateja <avschalapathirao@msitprogram.net>
2018-12-27 11:49:27 +0530

    "ads1_m12_a1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Remedial/Assignment-1/SeatAllotment.java
@@ -0,0 +1,123 @@
+/**
+ * Class for seat allotment.
+ */
+final class SeatAllotment implements Comparable<SeatAllotment> {
+	/**
+	 * Constructs the object.
+	 */
+	String name;
+	String dob;
+	int marks1;
+	int marks2;
+	int marks3;
+	int totalmarks;
+	String cate;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      name        The name
+	 * @param      dob         The dob
+	 * @param      marks1      The marks 1
+	 * @param      marks2      The marks 2
+	 * @param      marks3      The marks 3
+	 * @param      totalmarks  The totalmarks
+	 * @param      cate        The cate
+	 */
+	SeatAllotment(String name, String dob, int marks1, int marks2,
+		int marks3, int totalmarks, String cate) {
+		this.name = name;
+		this.dob = dob;
+		this.marks1 = marks1;
+		this.marks2 = marks2;
+		this.marks3 = marks3;
+		this.totalmarks = totalmarks;
+		this.cate = cate;
+	}
+	/**
+	 * totalmarks method.
+	 *
+	 * @return     int
+	 */
+	public int totalmarks() {
+		return totalmarks;
+	}
+	/**
+	 * dob method.
+	 *
+	 * @return     int
+	 */
+	public String dob() {
+		return dob;
+	}
+	/**
+	 * getname method.
+	 *
+	 * @return     int
+	 */
+	public String getname() {
+		return name;
+	}
+	/**
+	 * getcate method.
+	 *
+	 * @return     int
+	 */
+	public String cate() {
+		return cate;
+	}
+	/**
+	 * getmarks method.
+	 *
+	 * @return     int
+	 */
+	public int getmarks1() {
+		return marks1;
+	}
+	/**
+	 * getmarks method.
+	 *
+	 * @return     int
+	 */
+	public int getmarks2() {
+		return marks2;
+	}
+	/**
+	 * getmarks method.
+	 *
+	 * @return     int
+	 */
+	public int getmarks3() {
+		return marks3;
+	}
+	/**
+	 * compareTo method.
+	 *
+	 * @param      s     Seat Allotment
+	 *
+	 * @return     int
+	 */
+	public int compareTo(SeatAllotment s) {
+		if (this.totalmarks > s.totalmarks) {
+			return 1;
+		}
+		if (this.marks3 > s.marks3) {
+			return 1;
+		}
+		if (this.marks2 == s.marks2) {
+			return 1;
+		}
+		if (this.dob.equals(s.dob)){
+			return 1;
+		}
+		// if (s.cate.equals("SC")) {
+		// 	return 1;
+		// }
+		// if (s.cate.equals("ST")) {
+		// 	return 1;
+		// }
+		// if (s.cate.equals("BC")) {
+		// 	return 1;
+		// }
+		return 0;
+	}
+}
--- Remedial/Assignment-1/Solution.java
+++ Remedial/Assignment-1/Solution.java
@@ -1,17 +1,38 @@
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
 
-public class Solution {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int q = Integer.parseInt(scan.nextLine());
-		int v = Integer.parseInt(scan.nextLine());
-		int ur = Integer.parseInt(scan.nextLine());
-		int bc = Integer.parseInt(scan.nextLine());
-		int sc = Integer.parseInt(scan.nextLine());
-		int st = Integer.parseInt(scan.nextLine());
-		while(scan.hasNext()) {
-			String[] token = scan.nextLine().split(",");
-			System.out.println(token[0]+token[1]+token[2]+token[3]+token[4]);
-		}
 	}
+	/**
+	 * Main method.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Seat seat = new Seat();
+		int noofstu = Integer.parseInt(sc.nextLine());
+		int vac = Integer.parseInt(sc.nextLine());
+		int unresvac = Integer.parseInt(sc.nextLine());
+		int cateA = Integer.parseInt(sc.nextLine());
+		int cateB = Integer.parseInt(sc.nextLine());
+		int cateC = Integer.parseInt(sc.nextLine());
+		final int size = 10;
+		String[] str = new String[size];
+		for (int i = 0; i < noofstu; i++) {
+			str = sc.nextLine().split(",");
+			SeatAllotment s = new SeatAllotment(str[0], str[1], Integer.parseInt(str[2]),
+			Integer.parseInt(str[3]), Integer.parseInt(str[4]), Integer.parseInt(str[5]), str[6]);
+			seat.seatAllot(s);
+		}
+		seat.selectionSort();
+		System.out.println(seat + "\n");
+		seat.vacancies(vac);
+	}		
 }
